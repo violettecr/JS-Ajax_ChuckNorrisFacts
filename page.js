69fa5cc2d5da1fb14291eef094187e3580be6fd6
violettecr <violette.devweb@gmail.com>
2019-10-21 15:21:26 +0200

    quete ajax asynchrone Fetch Axios - 21/10/2019

---
+++ page.js
@@ -0,0 +1,11 @@
+
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+
+
+/*
+  appeler la bonne URL (section "Usage" de l'API)
+générer le HTML contenant l'avatar de Chuck et le texte, à partir de l'objet à partir de l'objet récupéré
+afficher ce bloc HTML dans la div appropriée
+Commit et push
+Dans l'onglet "Settings" de ton repo GitHub, active GitHub Pages sur la branche master.*/
